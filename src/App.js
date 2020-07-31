1f9371d41954c4d14401be49a772b0db23924144
hwong0305 <hwong.0305@gmail.com>
2020-07-30 19:22:33 -0700

    Add react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Editor from "./components/Editor";
 import Viewer from "./components/Viewer";
 import "./App.css";
 
 function App() {
-  return <Viewer />;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Viewer />
+        </Route>
+        <Route path="/notes/:id/edit">
+          <Editor />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
