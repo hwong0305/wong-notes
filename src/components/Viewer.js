9390134dc06aa406e35591e88d4ba54a53ffce3e
hwong0305 <hwong.0305@gmail.com>
2020-08-01 15:56:44 -0700

    Add logic to viewer

--- src/components/Viewer.js
+++ src/components/Viewer.js
@@ -1,7 +1,33 @@
-import React from "react";
-import "../styles/Viewer.css";
+import React, { useState, useEffect } from 'react'
+import showdown from 'showdown'
+import '../styles/Viewer.css'
+
+const converter = new showdown.Converter({
+  strikethrough: true,
+  tables: true
+})
 
 const Viewer = () => {
+  const [note, setNote] = useState('')
+  const [noteList, setNoteList] = useState([])
+
+  useEffect(() => {
+    fetch('https://hellosrv.devwong.com/api/notes')
+      .then(r => r.json())
+      .then(body => {
+        setNoteList(body)
+      })
+  }, [])
+
+  const handleSwitchNote = noteItem => {
+    fetch(`https://hellosrv.devwong.com/api/notes/${noteItem}`)
+      .then(r => r.json())
+      .then(body => {
+        body.body = converter.makeHtml(body.body)
+        setNote(body)
+      })
+  }
+
   return (
     <div className="container vh-100 overflow-hidden">
       <div className="row h-100">
@@ -18,41 +44,21 @@ const Viewer = () => {
               </div>
             </div>
           </div>
-          <div
-            className="list-group list-group-flush list-height"
-            id="fileList"
-          >
-            <button
-              type="button"
-              className="list-group-item list-group-item-action"
-            >
-              Cras justo odio
-            </button>
-            <button
-              type="button"
-              className="list-group-item list-group-item-action"
-            >
-              Dapibus ac facilisis in
-            </button>
-            <button
-              type="button"
-              className="list-group-item list-group-item-action"
-            >
-              Morbi leo risus
-            </button>
-            <button
-              type="button"
-              className="list-group-item list-group-item-action"
-            >
-              Porta ac consectetur ac
-            </button>
-            <button
-              type="button"
-              className="list-group-item list-group-item-action"
-              disabled
-            >
-              Vestibulum at eros
-            </button>
+          <div className="list-group list-group-flush list-height">
+            {noteList.map(noteItem => (
+              <button
+                type="button"
+                className={`list-group-item list-group-item-action ${
+                  noteItem === note.id ? 'active' : ''
+                }`}
+                onClick={() => {
+                  handleSwitchNote(noteItem)
+                }}
+                key={noteItem}
+              >
+                {noteItem}
+              </button>
+            ))}
           </div>
         </div>
         <div className="col-12 col-sm-8 border mh-100">
@@ -73,75 +79,11 @@ const Viewer = () => {
             </div>
           </div>
           <div className="row">
-            <div className="col-12 content-height">
-              <h1>HTML Ipsum Presents</h1>
-
-              <p>
-                <strong>Pellentesque habitant morbi tristique</strong> senectus
-                et netus et malesuada fames ac turpis egestas. Vestibulum tortor
-                quam, feugiat vitae, ultricies eget, tempor sit amet, ante.
-                Donec eu libero sit amet quam egestas semper.
-                <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend
-                leo. Quisque sit amet est et sapien ullamcorper pharetra.
-                Vestibulum erat wisi, condimentum sed,
-                <code>commodo vitae</code>, ornare sit amet, wisi. Aenean
-                fermentum, elit eget tincidunt condimentum, eros ipsum rutrum
-                orci, sagittis tempus lacus enim ac dui.
-                <a href="#">Donec non enim</a> in turpis pulvinar facilisis. Ut
-                felis.
-              </p>
-
-              <h2>Header Level 2</h2>
-
-              <ol>
-                <li>
-                  Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-                </li>
-                <li>Aliquam tincidunt mauris eu risus.</li>
-              </ol>
-
-              <blockquote>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Vivamus magna. Cras in mi at felis aliquet congue. Ut a est
-                  eget ligula molestie gravida. Curabitur massa. Donec eleifend,
-                  libero at sagittis mollis, tellus est malesuada tellus, at
-                  luctus turpis elit sit amet quam. Vivamus pretium ornare est.
-                </p>
-              </blockquote>
-
-              <h3>Header Level 3</h3>
-
-              <ul>
-                <li>
-                  Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-                </li>
-                <li>Aliquam tincidunt mauris eu risus.</li>
-              </ul>
-
-              <pre>
-                <code>
-                  #header h1 a {"{"}
-                  display: block; width: 300px; height: 80px;
-                  {"}"}
-                </code>
-              </pre>
-              <p>
-                Pellentesque habitant morbi tristique senectus et netus et
-                malesuada fames ac turpis egestas. Vestibulum tortor quam,
-                feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu
-                libero sit amet quam egestas semper. Aenean ultricies mi vitae
-                est. Mauris placerat eleifend leo. Quisque sit amet est et
-                sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum
-                sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum,
-                elit eget tincidunt condimentum, eros ipsum rutrum orci,
-                sagittis tempus lacus enim ac dui. Donec non enim in turpis
-                pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus
-                faucibus, tortor neque egestas augue, eu vulputate magna eros eu
-                erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis,
-                accumsan porttitor, facilisis luctus, metus
-              </p>
-            </div>
+            {/* showdownjs should be sanitizing html */}
+            <div
+              className="col-12 content-height"
+              dangerouslySetInnerHTML={{ __html: note.body }}
+            />
           </div>
         </div>
       </div>
@@ -180,7 +122,7 @@ const Viewer = () => {
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Viewer;
+export default Viewer
